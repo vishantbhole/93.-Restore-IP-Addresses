d2fa5a46953585b3c94827b7bee1325e4adbd22b
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-09-24 21:51:39 -0500

    Update restoreIpAddresses.py

--- restoreIpAddresses.py
+++ restoreIpAddresses.py
@@ -18,3 +18,9 @@ class Solution(object):
             if dots > 4:
                 return
 
+            for j in range(i, min(i + 3, len(s))):
+                    if( int(s[i:j + 1]) < 256 and(i == j or s[i] != "0")):
+                        backtrack(j + 1, dots + 1, currIP + s[i:j + 1] + ".")
+        backtrack(0,0,"")
+        return res
+
